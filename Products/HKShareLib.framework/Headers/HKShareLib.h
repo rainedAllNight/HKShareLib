8eaf49362d626f728f7eac991cadcf45a030299d
罗伟 <18356285339@163.com>
2017-06-29 10:32:28 +0800

    first commit

---
+++ Products/HKShareLib.framework/Headers/HKShareLib.h
@@ -0,0 +1,19 @@
+//
+//  HKShareLib.h
+//  HKShareLib
+//
+//  Created by 罗伟 on 2017/6/29.
+//  Copyright © 2017年 罗伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HKShareLib.
+FOUNDATION_EXPORT double HKShareLibVersionNumber;
+
+//! Project version string for HKShareLib.
+FOUNDATION_EXPORT const unsigned char HKShareLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HKShareLib/PublicHeader.h>
+
+
